138fe51d02115b901365a962b70c78fe8a4fb72e
samAricha <samaricha001@gmail.com>
2024-09-28 00:07:48 +0300

    fix(Request Location Permission separation): separating the RequestLocationPermission.kt logic to a separate file.

--- app/src/main/java/com/teka/googlemapsproject/presentation/location/LocationComposable.kt
+++ app/src/main/java/com/teka/googlemapsproject/presentation/location/LocationComposable.kt
@@ -8,9 +8,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.location.LocationServices
 
 @Composable
@@ -58,39 +55,7 @@ fun LocationComposable(context: Context) {
     }
 }
 
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun RequestLocationPermission(
-    onPermissionGranted: () -> Unit,
-    onPermissionDenied: () -> Unit,
-    onPermissionsRevoked: () -> Unit
-) {
-    val permissionState = rememberMultiplePermissionsState(
-        listOf(
-            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        )
-    )
 
-    LaunchedEffect(key1 = permissionState) {
-        val allPermissionsRevoked = permissionState.permissions.size == permissionState.revokedPermissions.size
-        val permissionsToRequest = permissionState.permissions.filter { !it.status.isGranted }
-
-        if (permissionsToRequest.isNotEmpty()) {
-            permissionState.launchMultiplePermissionRequest()
-        }
-
-        if (allPermissionsRevoked) {
-            onPermissionsRevoked()
-        } else {
-            if (permissionState.allPermissionsGranted) {
-                onPermissionGranted()
-            } else {
-                onPermissionDenied()
-            }
-        }
-    }
-}
 
 
 
---
+++ app/src/main/java/com/teka/googlemapsproject/presentation/location/RequestLocationPermission.kt
@@ -0,0 +1,41 @@
+package com.teka.googlemapsproject.presentation.location
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestLocationPermission(
+    onPermissionGranted: () -> Unit,
+    onPermissionDenied: () -> Unit,
+    onPermissionsRevoked: () -> Unit
+) {
+    val permissionState = rememberMultiplePermissionsState(
+        listOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    )
+
+    LaunchedEffect(key1 = permissionState) {
+        val allPermissionsRevoked = permissionState.permissions.size == permissionState.revokedPermissions.size
+        val permissionsToRequest = permissionState.permissions.filter { !it.status.isGranted }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            permissionState.launchMultiplePermissionRequest()
+        }
+
+        if (allPermissionsRevoked) {
+            onPermissionsRevoked()
+        } else {
+            if (permissionState.allPermissionsGranted) {
+                onPermissionGranted()
+            } else {
+                onPermissionDenied()
+            }
+        }
+    }
+}
